c95d97432fbab25294698d47da60233d273e6395
J-Ponzo <jeremy.ponzo@gmail.com>
2019-01-24 00:57:14 +0100

    first comit

---
+++ Assets/Scripts/GPUSkeletalAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUSkeletalAnimator : MonoBehaviour
+{
+    private Mesh mesh;
+
+    public float clipLength;
+    public float clipTime;
+    public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = this.GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clipTime += Time.deltaTime;
+        if (clipTime > clipLength)
+        {
+            clipTime = 0f;
+        }
+        t = clipTime / clipLength;
+
+        UpdateVertices();
+    }
+
+    private void UpdateVertices()
+    {
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            mesh.vertices[i].x = mesh.vertices[(i + 1) % mesh.vertexCount].x;
+            mesh.vertices[i].y = mesh.vertices[(i + 1) % mesh.vertexCount].x;
+            mesh.vertices[i].z = mesh.vertices[(i + 1) % mesh.vertexCount].x;
+        }
+    }
+}
---
+++ Assets/Scripts/SetupManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupManager : Singleton<SetupManager>
+{
+    protected SetupManager() { }
+
+    public GameObject prefab;
+    public int nbInsatances;
+    public float instanciationRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < nbInsatances; i++)
+        {
+            Vector2 position2D = Random.insideUnitCircle * instanciationRadius;
+            Vector3 position = new Vector3(position2D.x, 0f, position2D.y);
+            float angle = Random.Range(0f, 360f);
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+            GameObject instance = Instantiate(prefab, position, rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
