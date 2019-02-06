033d16eea37313157560adfac32106b9dabf6e48
jponzo <jponzo@wanadev.fr>
2019-02-06 14:00:40 +0100

    Anim baking on scriptable object from editor

---
+++ Assets/Scripts/GPUSkeletalAnim_Data.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GPUSkeletalAnim_Data", menuName = "GPUSkeletal/GPUAnim", order = 1)]
+public class GPUSkeletalAnim_Data : ScriptableObject
+{
+    //Input
+    public float samplingDelta = 0.1f;
+    public GameObject model;
+
+    //Baked Data
+    public float bake_samplingDelta;
+    public float bake_duration;
+    public int bake_nbBones; 
+    public Matrix4x4[] bake_data;
+
+
+    public void Bake()
+    {
+        //Get objects
+        GameObject skelModel = Instantiate(model);
+        Animator skelAnimator = skelModel.GetComponent<Animator>();
+        SkinnedMeshRenderer skelMeshRenderer = skelModel.GetComponentInChildren<SkinnedMeshRenderer>();
+        AnimationClip clip = skelAnimator.GetCurrentAnimatorClipInfo(0)[0].clip;
+
+        //Bake data
+        bake_samplingDelta = samplingDelta;
+        bake_duration = clip.length;
+        bake_nbBones = skelMeshRenderer.bones.Length;
+        bake_data = new Matrix4x4[bake_nbBones * (int) ((bake_duration / bake_samplingDelta) + 1)];
+        int idx = 0;
+        for (float t = 0; t < clip.length; t += samplingDelta)
+        {
+            skelAnimator.Update(samplingDelta);
+            for (int i = 0; i < skelMeshRenderer.bones.Length; i++)
+            {
+                Transform bone = skelMeshRenderer.bones[i];
+                bake_data[idx++] = bone.localToWorldMatrix;
+            }
+        }
+
+        DestroyImmediate(skelModel);
+
+        //Save data
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+#endif
+
+        Debug.Log("Baking Complete !");
+    }
+}
--- Assets/Scripts/GPUSkeletalDataBaker_Editor.cs
+++ Assets/Scripts/GPUSkeletalDataBaker_Editor.cs
@@ -3,53 +3,60 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(SetupManager))]
+[CustomEditor(typeof(GPUSkeletalAnim_Data))]
 public class GPUSkeletalDataBaker_Editor : Editor
 {
+
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        SetupManager setupManager = (SetupManager)target;
         if (GUILayout.Button("Bake Clip"))
         {
-            //Display curves
-            foreach (var binding in AnimationUtility.GetCurveBindings(setupManager.clip))
-            {
-                AnimationCurve curve = AnimationUtility.GetEditorCurve(setupManager.clip, binding);
-                Debug.Log(/*binding.path + "/" + */ binding.propertyName + "    nbKeys" + curve.keys.Length);
-            }
-
-            //Display rig
-            Mesh mesh = setupManager.prefab.GetComponentInChildren<MeshFilter>().sharedMesh;
-            //Debug.Log(mesh.bindposes.Length);
-            AssetImporter importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(setupManager.model));
-            ModelImporter modelImporter = importer as ModelImporter;
-            HumanBone[] humanBones = modelImporter.humanDescription.human;
-            foreach (HumanBone humanBone in humanBones)
-            {
-                //Debug.Log("bonename = " + humanBone.boneName + " humanName = " + humanBone.humanName);
-            }
-
-            for(int i = 0; i < HumanTrait.BoneName.Length; i++)
-            {
-                Debug.Log(HumanTrait.BoneName[i] + " => "
-                    + (HumanTrait.MuscleFromBone(i, 0) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 0)])) + ", "
-                    + (HumanTrait.MuscleFromBone(i, 1) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 1)])) + ", "
-                    + (HumanTrait.MuscleFromBone(i, 2) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 2)])) + ", "
-                    + (HumanTrait.MuscleFromBone(i, 3) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 3)])) + ", "
-                    + (HumanTrait.MuscleFromBone(i, 4) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 4)])) + ", "
-                    + (HumanTrait.MuscleFromBone(i, 5) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 5)])));
-            }
-            //Debug.Log("##########################################");
-            //for (int i = 0; i < HumanTrait.MuscleName.Length; i++)
-            //{
-            //    Debug.Log(HumanTrait.MuscleName[i]);
-            //}
-
-            //Transform root = FindRecursive(setupManager.model.transform, modelImporter.humanDescription.human[0].boneName);
-            //HumanPoseHandler handler = new HumanPoseHandler(setupManager.avatar, root);
-            //handler.
+            ((GPUSkeletalAnim_Data)target).Bake();
         }
+
+        //SetupManager setupManager = (SetupManager)target;
+        //if (GUILayout.Button("Bake Clip"))
+        //{
+        //    //Display curves
+        //    foreach (var binding in AnimationUtility.GetCurveBindings(setupManager.clip))
+        //    {
+        //        AnimationCurve curve = AnimationUtility.GetEditorCurve(setupManager.clip, binding);
+        //        Debug.Log(/*binding.path + "/" + */ binding.propertyName + "    nbKeys" + curve.keys.Length);
+        //    }
+
+        //    //Display rig
+        //    Mesh mesh = setupManager.prefab.GetComponentInChildren<MeshFilter>().sharedMesh;
+        //    //Debug.Log(mesh.bindposes.Length);
+        //    AssetImporter importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(setupManager.model));
+        //    ModelImporter modelImporter = importer as ModelImporter;
+        //    HumanBone[] humanBones = modelImporter.humanDescription.human;
+        //    foreach (HumanBone humanBone in humanBones)
+        //    {
+        //        //Debug.Log("bonename = " + humanBone.boneName + " humanName = " + humanBone.humanName);
+        //    }
+
+        //    for (int i = 0; i < HumanTrait.BoneName.Length; i++)
+        //    {
+        //        Debug.Log(HumanTrait.BoneName[i] + " => "
+        //            + (HumanTrait.MuscleFromBone(i, 0) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 0)])) + ", "
+        //            + (HumanTrait.MuscleFromBone(i, 1) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 1)])) + ", "
+        //            + (HumanTrait.MuscleFromBone(i, 2) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 2)])) + ", "
+        //            + (HumanTrait.MuscleFromBone(i, 3) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 3)])) + ", "
+        //            + (HumanTrait.MuscleFromBone(i, 4) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 4)])) + ", "
+        //            + (HumanTrait.MuscleFromBone(i, 5) == -1 ? "\n\tnull" : ("\n\t" + HumanTrait.MuscleName[HumanTrait.MuscleFromBone(i, 5)])));
+        //    }
+        //    //Debug.Log("##########################################");
+        //    //for (int i = 0; i < HumanTrait.MuscleName.Length; i++)
+        //    //{
+        //    //    Debug.Log(HumanTrait.MuscleName[i]);
+        //    //}
+
+        //    //Transform root = FindRecursive(setupManager.model.transform, modelImporter.humanDescription.human[0].boneName);
+        //    //HumanPoseHandler handler = new HumanPoseHandler(setupManager.avatar, root);
+        //    //HumanPose humanPose = new HumanPose();
+        //    //handler.GetHumanPose(ref humanPose);
+        //}
 
         //Transform FindRecursive(Transform root, string name)
         //{
